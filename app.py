a54a25e3b0cd4ca13a8bbe0824c98a245578a57a
Sikandar Nishad <109888740+sikandarnishad@users.noreply.github.com>
2023-11-09 11:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,98 @@
-import streamlit as st
-import pickle
-import pandas as pd
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IPL Win Predictor</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='styles.css') }}">
+    <style>
+        .form-container {
+            max-width: 400px;
+            margin: auto;
+        }
+        .form-group {
+            margin-bottom: 15px; /* Increased margin for more spacing */
+        }
+        
+        label {
+            font-weight: bold;
+            color:blue;
+            font-size: 25px; /* Increased font size */
+        }
+    </style>
+    
 
-teams = ['Sunrisers Hyderabad',
- 'Mumbai Indians',
- 'Royal Challengers Bangalore',
- 'Kolkata Knight Riders',
- 'Kings XI Punjab',
- 'Chennai Super Kings',
- 'Rajasthan Royals',
- 'Delhi Capitals']
+</head>
+<body>
+<h1 style="font-size:40px;text-align: center;color:black; margin: 10px;">Second Inning Win Pridiction</h1>
+<div style="background-image: url('https://resources.pulse.icc-cricket.com/photo-resources/2020/10/31/360fea86-e3f3-42f1-8ada-6fc1d82dd4c7/GettyImages-625462058.jpg?width=1920&height=1080');
+background-size: cover; height:500px; padding-top: 150px; text-align: center;">
+  
+  <form action="/predict" method="post">
 
-cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
-       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
-       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
-       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
-       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
-       'Sharjah', 'Mohali', 'Bengaluru']
+    
+    <div class="form-group">
+        <label for="batting_team">Select the batting team:</label>
+        <select name="batting_team" id="batting_team">
+            {% for team in teams %}
+                <option value="{{ team }}">{{ team }}</option>
+            {% endfor %}
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="bowling_team">Select the bowling team:</label>
+        <select name="bowling_team" id="bowling_team">
+            {% for team in teams %}
+                <option value="{{ team }}">{{ team }}</option>
+            {% endfor %}
+        </select>
+    </div>
 
-pipe = pickle.load(open('pipe.pkl','rb'))
-st.title('IPL Win Predictor')
 
-col1, col2 = st.beta_columns(2)
+    <div class="form-group">
+        <label for="selected_city">Select host city:</label>
+        <select name="selected_city" id="selected_city">
+            {% for city in cities %}
+                <option value="{{ city }}">{{ city }}</option>
+            {% endfor %}
+        </select>
+    </div>
 
-with col1:
-    batting_team = st.selectbox('Select the batting team',sorted(teams))
-with col2:
-    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
+    <div class="form-group">
+        <label for="target">Target:</label>
+        <input type="number" name="target" id="target" required>
+    </div>
 
-selected_city = st.selectbox('Select host city',sorted(cities))
+    <div class="form-group">
+        <label for="score">Score:</label>
+        <input type="number" name="score" id="score" required>
+    </div>
 
-target = st.number_input('Target')
+    <div class="form-group">
+        <label for="overs">Overs completed:</label>
+        <input type="number" name="overs" id="overs" required>
+    </div>
 
-col3,col4,col5 = st.beta_columns(3)
+    <div class="form-group">
+        <label for="wickets">Wickets out:</label>
+        <input type="number" name="wickets" id="wickets" required>
+    </div>
 
-with col3:
-    score = st.number_input('Score')
-with col4:
-    overs = st.number_input('Overs completed')
-with col5:
-    wickets = st.number_input('Wickets out')
+    <div class="form-group">
+        <input type="submit" value="Predict Probability">
+    </div>
+</form>
+<!-- 
+{% if result %}
+        <h2>{{ result['batting_team'] }} - {{ result['win_probability'] }}%</h2>
+        <h2>{{ result['bowling_team'] }} - {{ result['loss_probability'] }}%</h2>
+    {% endif %}
+    -->
+    {% if result %}
+    <h2 style="color: yellow;">{{ result['batting_team'] }} - {{ result['win_probability'] }}%</h2>
+    <h2 style="color: yellow;">{{ result['bowling_team'] }} - {{ result['loss_probability'] }}%</h2>
+{% endif %}
 
-if st.button('Predict Probability'):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
-
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
-
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+</div>
+</body>
+</html>
